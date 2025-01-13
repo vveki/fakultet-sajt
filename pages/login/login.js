ae5169b6e3f5144fb5a6815083a33605067e852b
vveki <93678920+vveki@users.noreply.github.com>
2025-01-13 11:13:19 +0100

    testt

--- pages/login/login.js
+++ pages/login/login.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let users = [];
 
-    fetch('/pages/login/users.json')
+    fetch('../pages/login/users.json')
         .then(response => response.json())
         .then(data => {
             users = data;
